61c67565b2b13849ead2e2f52bcd3813b2546849
Muhammedaliolgun <162563170+Muhammedaliolgun@users.noreply.github.com>
2024-03-08 15:16:04 +0300

    Add files via upload

---
+++ switch 10.video/src/v10.java
@@ -0,0 +1,29 @@
+
+public class v10 {
+
+	public static void main(String[] args) { 
+		char grade ='A';
+		
+		switch(grade) {
+			case 'A':
+				System.out.println("Mükemmel : Geçtiniz");
+				break;
+			case 'B' :
+			case 'C' : 
+				System.out.println("İyi : Geçtiniz");
+				break;
+			case 'D' :
+				System.out.println("Fena Değil  : Geçtiniz");
+				break;
+			case 'F' :
+				System.out.println("Malesef Kaldınız");
+				break;
+				default:
+					System.out.println("Geçersiz Not Girdiniz");
+				
+		
+		
+		}
+		}
+
+}
---
+++ veriables/src/veriables/v9.java
@@ -0,0 +1,25 @@
+package veriables;
+
+public class v9 {
+
+	public static void main(String[] args) {
+		 int sayı1 =24;
+		 int sayı2 =25;
+		 int sayı3 =2;
+		 int enBuyuk = sayı1;
+		 
+		 if(enBuyuk<sayı2) {
+			 enBuyuk = sayı2;
+		 }
+	  
+	     if(enBuyuk<sayı3) {
+	    	 enBuyuk = sayı3;
+	     }
+	  
+	     System.out.println("En büyük ="+enBuyuk);
+	     
+	    
+		}
+
+	}
+	 
